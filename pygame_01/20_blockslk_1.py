c51741dd700597c05445a2513439ac052889da05
hyunjilee0729 <hyunjilee729>
2022-07-22 18:48:03 +0900

    7/22 벽돌꺠기 2번쨰 완료

--- pygame_01/20_blockslk_1.py
+++ pygame_01/20_blockslk_1.py
@@ -18,6 +18,7 @@ bar_xPos = screen_width / 2 - bar_width / 2
 bar_yPos = screen_height - bar_height
 
 bar_rect = pygame.Rect(bar_xPos, bar_yPos, bar_width, bar_height)
+bar_to_X = 0
 
 ball_size = 20
 
@@ -25,6 +26,10 @@ ball_xPos = screen_width / 2
 ball_yPos = screen_height - bar_height - ball_size
 
 ball_rect = pygame.Rect(ball_xPos, ball_yPos, ball_size * 2, ball_size * 2)
+ball_rect.center = (ball_xPos, ball_yPos)
+
+ball_x_speed = 0.3
+ball_y_speed = 0.3
 
 block_width = screen_width / 10
 block_height = screen_height / 20
@@ -43,21 +48,63 @@ for i in range(10):
 print(blocks)
 print(block_color)
 
+mouse_xPos = 0
+mouse_yPos = 0
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False     
+            running = False  
+
+        if event.type == pygame.MOUSEMOTION:
+            mouse_xPos, mouse_yPos = pygame.mouse.get_pos()
+            if mouse_xPos - bar_width / 2 >= 0 and mouse_xPos + bar_width / 2 <= screen_width:
+                bar_xPos = mouse_xPos - bar_width / 2
+                bar_rect.left = mouse_xPos - bar_width / 2   
 
     screen.fill((200, 200, 100))
 
+    bar_xPos += bar_to_X
+
+    if bar_xPos < 0:
+        bar_xPos = 0
+    if bar_xPos > screen_width - bar_width:
+        bar_xPos = screen_width - bar_width
+    bar_rect.left = bar_xPos
+
     pygame.draw.rect(screen, (90, 90, 255), bar_rect)
 
+    if ball_xPos - ball_size <= 0:
+        ball_x_speed = -ball_x_speed
+    elif ball_xPos >= screen_width - ball_size:
+        ball_x_speed = -ball_x_speed
+
+    if ball_yPos - ball_size <= 0:
+        ball_y_speed = -ball_y_speed
+    elif ball_yPos >= screen_width - ball_size:
+        ball_y_speed = -ball_y_speed
+
+    ball_xPos += ball_x_speed
+    ball_yPos +=ball_y_speed
+
+    ball_rect.center = (ball_xPos, ball_yPos)
     pygame.draw.circle(screen, (255, 255, 255), (ball_xPos, ball_yPos), ball_size)
 
+    if ball_rect.colliderect(bar_rect):
+        ball_y_speed *= -1
+
+
     for i in range(10):
-        for j in range(3):    
-            pygame.draw.rect(screen, block_color[i][j], blocks[i][j])
-    pygame.display.update()    
+        for j in range(3): 
+            if blocks[i][j]:
+                pygame.draw.rect(screen, block_color[i][j], blocks[i][j])
+                blocks[i][j].topleft = (i * block_width, j * block_height)
+
+                if ball_rect.colliderect(blocks[i][j]):
+                    ball_x_speed *= -1
+                    ball_y_speed *= -1
+                    blocks[i][j] = 0
+    pygame.display.update()
 
 pygame.quit()
