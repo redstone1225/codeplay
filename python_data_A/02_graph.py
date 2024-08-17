98a5b62d9fd258bcf561a8aa9fbca35412625721
redstone1225 <alexchoi1225@gmail.com>
2024-08-17 12:36:39 +0900

    그래프 그리기 기초

---
+++ python_data_A/02_graph.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.title('codefuck')
+plt.plot([10, 20, 30, 40], 'r.', label='circle')
+plt.plot([40, 30, 20, 10], 'g^', label='triangle up')
+plt.legend()
+plt.show()
