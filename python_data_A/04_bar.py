7dd1f3c14b4eb491e30167836856daa43549803b
redstone1225 <alexchoi1225@gmail.com>
2024-12-07 12:32:03 +0900

    12/07 양평 노인 인구 비율

--- python_data_A/04_bar.py
+++ python_data_A/04_bar.py
@@ -1,19 +1,43 @@
 # https://www.mois.go.kr/
 import matplotlib.pyplot as plt
 import csv
-# plt.bar([0, 1, 2, 4, 6, 10], [1, 2, 3, 5, 6, 7])
-plt.barh(range(6), [1, 2, 3, 5, 6, 7])
-plt.show()
 
-
-# f = open('', 'r', encoding='utf8')
-# data = csv.reader(f)
-# result = []
+f = open('python_data_A/csv_data/yp10.csv', 'r', encoding='utf8')
+data = csv.reader(f)
+result = []
 # for row in data :
 #     if'양평읍' in row[0] :
 #         for i in row[3:] :
+#             if ',' in i:
+#                 i = i.replace(',', '')
 #             result.append(int(i))
 
-# plt.bar(range(101), result)
-# plt.barh(range(101), result)
+# plt.bar(range(len(result)), result)
+# # plt.barh(range(len(result)), result)
 # plt.show()
+yp_all = []
+label_ages = ["0~9", "10~19", "20~29", "30~39", "4-~49", "50~59", "60~69", "70~79", "80~89", "90~99", "100~"]
+label_gen = ["청소년(0~19)", "청년(20~39)", "장년(40~59)", "노년(60~)"]
+yp_gen = []
+
+
+for i in data:
+    yp_all.append(i)
+
+for da in range(len(yp_all)):
+    for change in range(len(yp_all[da][3:])):
+        if "," in yp_all[da][change + 3]:
+            yp_all[da][change + 3] = int(yp_all[da][change + 3].replace(",",""))
+        else:
+            yp_all[da][change + 3] = int(yp_all[da][change + 3])
+
+# plt.pie(yp_all[10][3:], labels=label_ages, autopct="%.1f%%")
+yp_gen.append(sum(yp_all[0][3:5]))
+yp_gen.append(sum(yp_all[0][5:7]))
+yp_gen.append(sum(yp_all[0][7:9]))
+yp_gen.append(sum(yp_all[0][9:]))
+
+plt.pie(yp_gen, labels=label_gen, autopct="%.1f%%")
+plt.show()
+
+
